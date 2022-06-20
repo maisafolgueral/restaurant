e6f9f866363409ad614cfa5065fee862a0d0e0fe
Maisa <seu e-mail>
2022-06-20 15:27:20 -0300

    Created Lateral Menu

---
+++ src/components/CategoryMenu.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="category-menu">
+    <ul>
+      <li
+        v-for="category in categoriesList"
+        :key="category.id"
+        @click="onCategoryClick(category.id)" 
+        :class="{'active': isActive(category.id)}"
+      >
+        <component :is="category.icon" />
+        <p>{{ category.label }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Pizza from "../assets/icons/pizza.svg";
+import Combo from "../assets/icons/french-fries.svg";
+import Deserts from "../assets/icons/ice-cream.svg";
+import Drinks from "../assets/icons/smothie.svg";
+import Burguer from "../assets/icons/food.svg";
+
+export default {
+  name: "CategoryMenu",
+  components: {
+    Pizza,
+    Combo,
+    Deserts,
+    Drinks,
+    Burguer,
+  },
+  data() {
+    return {
+      categoriesList: [
+        { label: "Pizza", icon: "Pizza", id: "pizza" },
+        { label: "Bebidas", icon: "Drinks", id: "drinks" },
+        { label: "Doces", icon: "Deserts", id: "deserts" },
+        { label: "Combos", icon: "Combo", id: "combo" },
+        { label: "Burguers", icon: "Burguer", id: "burguer" },
+      ],
+      selectedCategory: "pizza",
+    };
+  },
+  methods: {
+    onCategoryClick(id) {
+      this.selectedCategory = id;
+    },
+    isActive(id) {
+        return this.selectedCategory === id;
+    }
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.category-menu {
+  width: 130px;
+  height: 100vh;
+  background: #fff;
+
+  display: flex;
+  align-items: center;
+
+  ul {
+    list-style: none;
+    padding: 0;
+    width: 100%;
+
+    li {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      height: 100px;
+
+      p {
+        margin-bottom: 0px;
+        font-weight: 500;
+        font-size: 14px;
+        color: @dark-grey;
+      }
+
+      svg {
+        path {
+            fill: @dark-grey;
+        }
+      }
+
+      &.active {
+        background: @light-yellow;
+        border-radius: 8px;
+
+        p {
+            color: #000;
+        }
+
+        svg {
+            path {
+                fill: #000;
+            }
+        }
+      }
+    }
+  }
+
+  @media @tablets {
+    width: 100%;
+    height: fit-content;
+
+    ul {
+        display: flex;
+        justify-content: center;
+        margin: 20px;
+        overflow: scroll;
+
+        li {
+            min-width: 78px;
+        }
+    }
+  }
+}
+</style>
