4afca276c8483ed73aebd7ed2f074617d7dcaf9a
Maisa <seu e-mail>
2022-06-20 16:21:47 -0300

    Added Json Serve

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,17 @@
 <script>
 // @ is an alias to /src
 import CategoryMenu from '@/components/CategoryMenu.vue'
+import axios from 'axios';
 
 export default {
   name: 'HomeView',
   components: {
     CategoryMenu
+  },
+  created() {
+    axios.get('http://localhost:3000/burguers').then( response => {
+      console.log(response);
+    })
   }
 }
 </script>
