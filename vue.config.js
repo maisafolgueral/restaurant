101d80d984bad5cb9940983fe4e4a67a8115749c
Maisa <seu e-mail>
2022-06-20 12:26:54 -0300

    Loaded SVG

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,27 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+});
 
 module.exports = {
   css: {
     loaderOptions: {
       less: {
-        additionalData: `@import "@/assets/css/variables.less";`
-      }
-    }
+        additionalData: `@import "@/assets/css/variables.less";`,
+      },
+    },
+  },
+  chainWebpack: config => {
+    config.module.rules.delete("svg");
+  },
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.svg$/, 
+          loader: 'vue-svg-loader', 
+        },
+      ],
+    }      
   }
-}
+};
