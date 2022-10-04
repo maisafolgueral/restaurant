e9b87ab701bacfeeb140ba33ff53874096376ebe
Maisa <seu e-mail>
2022-10-04 17:48:42 -0300

    Created Payment

--- src/components/CartRestaurant.vue
+++ src/components/CartRestaurant.vue
@@ -2,18 +2,21 @@
   <div class="cart">
     <router-link to="/" class="cart--go-back" v-if="isSmallScreens()">←️ Voltar</router-link>
     <h2 class="cart--title">Seu pedido</h2>
-    <p v-if="hasNoItem">Seu carrinho ainda está vazio</p>
-    <transition-group name="list">
-      <CartItem 
-      v-for="item in cartList" 
-      :key="item.id"
-      :item="item"
-      />
-    </transition-group>
+    <div class="cart--content">
+      <p v-if="hasNoItem">Seu carrinho ainda está vazio</p>
+      <transition-group name="list">
+        <CartItem 
+        v-for="item in cartList" 
+        :key="item.id"
+        :item="item"
+        />
+      </transition-group>
+    </div>
     <div class="cart--total" v-if="!hasNoItem">
       <span>Total: </span>
       <span class="price">{{getCartTotal | currency}}</span>
-    </div>
+      </div>
+    <button class="primary-button payment-button" @click="goToPayment">Finalizar compra</button>
   </div>
 </template>
 
@@ -45,6 +48,11 @@ export default {
     hasNoItem() {
       return !this.cartList.length;
     },
+  },
+  methods: {
+    goToPayment() {
+      this.$router.push({name: 'PaymentView'});
+    }
   }
 }
 </script>
@@ -53,8 +61,11 @@ export default {
   .cart {
     background: #FFF;
     width: 643px;
+    height: 100vh;
     min-width: 643px;
     padding: 50px;
+    display: flex;
+    flex-direction: column;
 
     &--go-back {
       font-weight: 600;
@@ -69,6 +80,11 @@ export default {
       font-size: 24px;
     }
 
+    &--content {
+      flex-grow: 1;
+      overflow: auto;
+    }
+
     &--total {
       font-weight: 600;
       font-size: 18px;
@@ -79,6 +95,11 @@ export default {
         color: @yellow;
         padding-left: 10px;
       }
+    }
+
+    .payment-button {
+      width: 397px;
+      margin: 20px auto;
     }
 
     .list-enter-active, 
@@ -94,6 +115,10 @@ export default {
       width: 100%;
       min-width: unset;
       padding: 50px 20px 20px;
+
+      .payment-button {
+        width: 100%;
+      }
     }
   }
 </style>
