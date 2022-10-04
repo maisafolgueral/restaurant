92b7c55f9ba69e9bd42f1b6c2488133940ae2dc2
Maisa <seu e-mail>
2022-10-04 18:11:18 -0300

    Updated payment

--- src/views/PaymentView.vue
+++ src/views/PaymentView.vue
@@ -1,15 +1,40 @@
 <template>
-  <div>
-    pagamento
+  <div class="payment">
+    <div class="payment--content">
+      <router-link to="/" class="payment--go-back">←️ Voltar</router-link>
+    </div>
+    <CartRestaurant v-if="isDesktop()"/>
   </div>
 </template>
 
 <script>
-export default {
+import CartRestaurant from "@/components/CartRestaurant.vue";
+import Mixin from "@/mixins/mixins";
 
+export default {
+  components: {
+    CartRestaurant
+  },
+  mixins: [Mixin]
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+
+.payment {
+  display: flex;
+
+  &--content {
+    flex-grow: 1;
+    padding: 50px;
+
+    .payment--go-back {
+      font-weight: 600;
+      font-size: 18px;
+      text-decoration: none;
+      color: #000;
+    }
+  }
+}
 
 </style>
